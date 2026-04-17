5a23f0fc132a15bcdcf8ed0b7ef1d3eee6973978
PRAGHATHAPRIYA2589 <126149818+PRAGHATHAPRIYA2589@users.noreply.github.com>
2026-04-17 08:33:45 +0530

    Add unit master page UI template

---
+++ unit.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>JPSN-Unit Master </title>
+    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon.png">
+    <link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+    <div id="preloader">
+        <div class="sk-three-bounce">
+            <div class="sk-child sk-bounce1"></div>
+            <div class="sk-child sk-bounce2"></div>
+            <div class="sk-child sk-bounce3"></div>
+        </div>
+    </div>
+
+    <div id="main-wrapper">
+        <div class="content-body">
+            <div class="container-fluid">
+                <div class="row page-titles mx-0">
+                    <div class="col-sm-6 p-md-0">
+                        <div class="welcome-text">
+                            <h4>Master</h4>
+                            <span class="ml-1">Unit Master</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xl-6 col-xxl-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h4 class="card-title">Unit Master</h4>
+                            </div>
+                            <div class="card-body">
+                                <div class="basic-form">
+                                    <form method="post" action="/admin/unit.php">
+                                        <div class="form-group">
+                                            <input type="text" class="form-control input-default" required placeholder="Unit name" id="txtUnitMasterID" name="txtUnitmastername">
+                                        </div>
+                                        <div class="form-group">
+                                            <button type="submit" class="btn btn-primary" name="btnSave">Save</button>
+                                            <button type="reset" class="btn btn-primary" name="btnReset">Reset</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table primary-table-bordered">
+                            <thead class="thead-primary">
+                                <tr>
+                                    <th scope="col">S.No</th>
+                                    <th scope="col">Unit Name</th>
+                                    <th scope="col">Edit</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr><th>15</th><th>GRAM</th>
+                                    <td><span><a href="unitedit.php?Sid=15" class="mr-4" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fa fa-pencil color-muted"></i></a></span></td>
+                                </tr>
+                                <tr><th>14</th><th>SET</th>
+                                    <td><span><a href="unitedit.php?Sid=14" class="mr-4" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fa fa-pencil color-muted"></i></a></span></td>
+                                </tr>
+                                <tr><th>13</th><th>BAG</th>
+                                    <td><span><a href="unitedit.php?Sid=13" class="mr-4" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fa fa-pencil color-muted"></i></a></span></td>
+                                </tr>
+                                <tr><th>12</th><th>FEET</th>
+                                    <td><span><a href="unitedit.php?Sid=12" class="mr-4" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fa fa-pencil color-muted"></i></a></span></td>
+                                </tr>
+                                <tr><th>11</th><th>LTR</th>
+                                    <td><span><a href="unitedit.php?Sid=11" class="mr-4" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fa fa-pencil color-muted"></i></a></span></td>
+                                </tr>
+                                <tr><th>10</th><th>BOX</th>
+                                    <td><span><a href="unitedit.php?Sid=10" class="mr-4" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fa fa-pencil color-muted"></i></a></span></td>
+                                </tr>
+                                <tr><th>9</th><th>LENGTH</th>
+                                    <td><span><a href="unitedit.php?Sid=9" class="mr-4" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fa fa-pencil color-muted"></i></a></span></td>
+                                </tr>
+                                <tr><th>8</th><th>PKT</th>
+                                    <td><span><a href="unitedit.php?Sid=8" class="mr-4" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fa fa-pencil color-muted"></i></a></span></td>
+                                </tr>
+                                <tr><th>7</th><th>SQFT</th>
+                                    <td><span><a href="unitedit.php?Sid=7" class="mr-4" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fa fa-pencil color-muted"></i></a></span></td>
+                                </tr>
+                                <tr><th>6</th><th>COIL</th>
+                                    <td><span><a href="unitedit.php?Sid=6" class="mr-4" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fa fa-pencil color-muted"></i></a></span></td>
+                                </tr>
+                                <tr><th>5</th><th>MTR</th>
+                                    <td><span><a href="unitedit.php?Sid=5" class="mr-4" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fa fa-pencil color-muted"></i></a></span></td>
+                                </tr>
+                                <tr><th>4</th><th>ROLL</th>
+                                    <td><span><a href="unitedit.php?Sid=4" class="mr-4" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fa fa-pencil color-muted"></i></a></span></td>
+                                </tr>
+                                <tr><th>3</th><th>KG</th>
+                                    <td><span><a href="unitedit.php?Sid=3" class="mr-4" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fa fa-pencil color-muted"></i></a></span></td>
+                                </tr>
+                                <tr><th>2</th><th>Nos</th>
+                                    <td><span><a href="unitedit.php?Sid=2" class="mr-4" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fa fa-pencil color-muted"></i></a></span></td>
+                                </tr>
+                                <tr><th>1</th><th>PC</th>
+                                    <td><span><a href="unitedit.php?Sid=1" class="mr-4" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fa fa-pencil color-muted"></i></a></span></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer">
+            <div class="copyright">
+                <p>Copyright © Designed &amp; Developed by <a href="astonishinfotech.com" target="_blank">Astonish InfoTech</a> 2024</p>
+            </div>
+        </div>
+    </div>
+    <script src="vendor/global/global.min.js"></script>
+    <script src="js/quixnav-init.js"></script>
+    <script src="js/custom.min.js"></script>
+</body>
+</html>
